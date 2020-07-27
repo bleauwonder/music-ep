24e30688cb16502b684784936ad696264b41e209
bleauwonder <elyzableau.om@gmail.com>
2020-07-27 11:55:51 -0400

    add typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
       <Link to="contact/">Contact</Link>
       <Header headerText="Elyza Bleau" />
-      <p>Haunted EP</p>
+      <h2>Haunted EP</h2>
       <img src="/images/001-Mother-and-Lillian.jpg"/>
+      <p>I'm so excited to rock with you</p>
     </div>
   )
 }
